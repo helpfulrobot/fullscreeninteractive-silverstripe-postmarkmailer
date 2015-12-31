1053c5ac3c65f89318146faff3e184fcb09cd573
helpfulrobot <chris@silverstripe.com>
2016-01-01 01:45:51 +1300

    Converted to PSR-2

--- tests/PostmarkMailerTest.php
+++ tests/PostmarkMailerTest.php
@@ -5,38 +5,40 @@
  * @subpackage tests
  */
 
-class PostmarkMailerTest extends SapphireTest {
-	
-	function testParseEmailString() {
-		$emails = array(
-			'foo@bar.com' => 'foo@bar.com',
-			'<foo@bar.com>' => 'foo@bar.com',
-			'Baz <foo@bar.com>' => 'foo@bar.com'
-		);
-		
-		foreach($emails as $string => $email) {
-			$results = PostmarkMailer::parse_email_addresses($string);
+class PostmarkMailerTest extends SapphireTest
+{
+    
+    public function testParseEmailString()
+    {
+        $emails = array(
+            'foo@bar.com' => 'foo@bar.com',
+            '<foo@bar.com>' => 'foo@bar.com',
+            'Baz <foo@bar.com>' => 'foo@bar.com'
+        );
+        
+        foreach ($emails as $string => $email) {
+            $results = PostmarkMailer::parse_email_addresses($string);
 
-			$this->assertEquals(1, count($results));
-			$this->assertEquals($email, $results[0]);
-		}
-		
-		$complex = array(
-			'foo@bar.com, bar@foo.com' => array(
-				'foo@bar.com',
-				'bar@foo.com'
-			),
-			'Baz <foo@bar.com>, bar@foo.com' => array(
-				'foo@bar.com',
-				'bar@foo.com'
-			)
-		);
-		
-		foreach($complex as $string => $emails) {
-			$results = PostmarkMailer::parse_email_addresses($string);
-			
-			$this->assertEquals(2, count($results));
-			$this->assertEquals($emails, $results);
-		}
-	}
+            $this->assertEquals(1, count($results));
+            $this->assertEquals($email, $results[0]);
+        }
+        
+        $complex = array(
+            'foo@bar.com, bar@foo.com' => array(
+                'foo@bar.com',
+                'bar@foo.com'
+            ),
+            'Baz <foo@bar.com>, bar@foo.com' => array(
+                'foo@bar.com',
+                'bar@foo.com'
+            )
+        );
+        
+        foreach ($complex as $string => $emails) {
+            $results = PostmarkMailer::parse_email_addresses($string);
+            
+            $this->assertEquals(2, count($results));
+            $this->assertEquals($emails, $results);
+        }
+    }
 }
